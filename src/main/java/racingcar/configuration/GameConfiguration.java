ca5ca5fb1dd26ca0c67f992aee5940c99e90bdda
devdynam0507 <wsnam0507@naver.com>
2023-10-26 16:59:26 +0900

    feat: 자동차 DI 콘피그 클래스에 Configuration 어노테이션 추가

--- src/main/java/racingcar/configuration/GameConfiguration.java
+++ src/main/java/racingcar/configuration/GameConfiguration.java
@@ -1,8 +1,10 @@
 package racingcar.configuration;
 
 import framework.dependency.Component;
+import framework.dependency.Configuration;
 import racingcar.domain.Cars;
 
+@Configuration
 public class GameConfiguration {
 
     @Component(name = "cars")
