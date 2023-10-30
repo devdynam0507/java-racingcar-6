dd81770dc4acf33512115986ebe03bfd74e905d7
devdynam0507 <wsnam0507@naver.com>
2023-10-30 20:26:31 +0900

    refactor: CarService 삭제

--- src/main/java/racingcar/configuration/GameConfiguration.java
+++ src/main/java/racingcar/configuration/GameConfiguration.java
@@ -4,7 +4,6 @@ import racingcar.framework.dependency.Component;
 import racingcar.framework.dependency.Configuration;
 
 import racingcar.domain.CarRepository;
-import racingcar.service.CarService;
 
 @Configuration
 public class GameConfiguration {
@@ -12,10 +11,5 @@ public class GameConfiguration {
     @Component(name = "carRepository")
     public CarRepository cars() {
         return new CarRepository();
-    }
-
-    @Component(name = "carService")
-    public CarService carService(CarRepository carRepository) {
-        return new CarService(carRepository);
     }
 }
